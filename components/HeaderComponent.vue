feb4cc10046035c77f9ebd77b212337c1869e12e
abbjetmus <jeton.mustini@abbgymnasiet.se>
2022-03-10 16:27:00 +0100

    initial commit

---
+++ components/HeaderComponent.vue
@@ -0,0 +1,5 @@
+<template>
+<header>
+    <h1>My Food</h1>
+</header>
+</template>
---
+++ layouts/default.vue
@@ -0,0 +1,6 @@
+<template>
+<div class="container mx-auto">
+    <HeaderComponent></HeaderComponent>
+    <nuxt/>
+</div>
+</template>
